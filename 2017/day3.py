d68a84cbc4edc793e223e0818bb7e2e9572b56c1
sn0wcr@sh <syedomair0@gmail.com>
2023-12-04 00:11:10 -0600

    finished day 3

---
+++ 2017/day3.py
@@ -0,0 +1,16 @@
+inp = 289326
+
+
+width = 2
+l = []
+x, y = 0,0
+found = False
+
+start, last = 1,0
+while not found:
+    perimeter = 4 * width
+    range(start, start + perimeter)
+
+    
+    
+    w += 2
